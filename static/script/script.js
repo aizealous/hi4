99c4011181c50a92513f5e55d3c4c31c795f7b92
Nelson Siu <NSIU@grupocargo.com>
2024-06-15 12:31:41 -0300

    new voice tts

--- static/script/script.js
+++ static/script/script.js
@@ -1,3 +1,28 @@
 function myOnload() {
-alert("TEN MUCHA PRECAUCIÓN\nEL CONTENIDO DE LA WEB PUEDE CAUSAR BALANCES ENERGÉTICOS y ARMONÍA\nPRESIONAR EL BOTÓN ACEPTAR ES ARRIESGARSE A EXPLORAR UN CONTENIDO QUE TE ALEGRE Y MOTIVE MUCHO EN ESPECIAL LOS DÍAS DE QUILOMBOS\nO AL MENOS TE VAS A REIR SI TE PARECE UNA TONTERÍA LA WEB\n\n                          \uD83D\uDE00 🧘🧘🧘 \uD83D\uDE00");
+alert("TEN MUCHA PRECAUCIÓN\nEL CONTENIDO DE LA WEB PUEDE CAUSAR BALANCES ENERGÉTICOS y ARMONÍA\nPRESIONAR EL BOTÓN ACEPTAR ES ARRIESGARSE A EXPLORAR UN CONTENIDO QUE TE ALEGRE Y MOTIVE MUCHO EN ESPECIAL LOS DÍAS DE QUILOMBOS\nO AL MENOS TE VAS A BURLAR SI TE PARECE UNA TONTERÍA LA WEB\n\n                          \uD83D\uDE00 🧘🧘🧘 \uD83D\uDE00");
  }
+
+
+ const vibrateImg = () => {
+   window.navigator.vibrate([500,100,1000,50,500,200,1000,50,500, 100, 1000, 200, 500, 100, 1000,50, 500]);
+   //const backgr=document.querySelector("body");
+   //backgr.style.backgroundColor="white";
+   const text="HOLA SOY YOGIWEB, PODES ESCUCHARME SIN AURICULARES, NO SOY DE AMAZON, NI DE TESLA, PERO ESPERO SERVIRTE PARA ESTUDIAR, SONREIR, HACER YOGA, HOLGAZANEAR, ETC";
+   const namaste=document.getElementsByClassName("namaste")[0];
+   namaste.innerHTML=text;
+   const nodeList = document.querySelectorAll("img");
+   for (let i = 0; i < nodeList.length; i++) {
+      nodeList[i].style.boxShadow= "0px 0px 500px burlywood";
+      }
+
+
+   var msg = new SpeechSynthesisUtterance();
+   var voices = window.speechSynthesis.getVoices();   
+   msg.voice = voices.filter(function(voice) { return voice.lang == 'es-ES' && voice.gender == 'female'; })[0];
+   msg.text = text;
+   msg.pitch = 0.2;
+   msg.rate = 0.8;
+   console.log(msg.text );
+   speechSynthesis.speak(msg);
+   }
+   
